c65f7f0bebb525b05dc73aa998b591dada49bc06
Dziad573 <krystiansmagacz05@gmail.com>
2024-04-02 14:45:38 +0200

    vue

---
+++ Vue/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <button @click='incrementPension'>++</button>
+  <p>Your pension: {{pension}}</p>
+
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'HellowWorld',
+
+  setup(){
+    let pension = ref(10);
+
+    function incrementPension(){
+      pension.value++;
+    }
+    return {pension, incrementPension};
+  }
+  
+
+}
+</script>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
