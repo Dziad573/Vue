1d372e7b2326f7b61bfb175a067a4a815f7fa857
Dziad573 <krystiansmagacz05@gmail.com>
2024-04-02 15:25:50 +0200

    reactive()

--- Vue/src/components/HelloWorld.vue
+++ Vue/src/components/HelloWorld.vue
@@ -3,11 +3,17 @@
   <button @click='incrementPension'>++</button>
   <p>Your pension: {{pension}}</p>
 
+  <h2>Price: {{ state.price }}</h2>
+  <button @click="makeOrder">Make next order</button>
+  <p>Quantity: {{ state.quantity }}</p>
+  <p>Order total price: {{ state.totalPrice }} {{  state.quantity < 14 ? "": " -5%"  }}</p>
+  <p>Tax {{ state.tax }}</p>
+
 </template>
 
 
 <script>
-import { ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 export default {
   name: 'HellowWorld',
@@ -18,11 +24,35 @@ export default {
     function incrementPension(){
       pension.value++;
     }
-    return {pension, incrementPension};
+
+    const state = reactive({
+      quantity: 10,
+      price: 100,
+
+      totalPrice: computed(() => {
+        if (state.quantity < 14){
+          return state.quantity * state.price;
+        }else{
+          return state.quantity * (state.price - 5);
+        }
+      }),
+      tax: computed(() => {
+        return Math.round(state.totalPrice * 0.23);
+      }),
+    });
+    
+    function makeOrder(){
+      state.quantity++;
+    }
+    
+    
+
+
+    return {pension, incrementPension, state, makeOrder};
   }
   
 
-}
+};
 </script>
 
 
