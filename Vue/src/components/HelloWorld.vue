5e1284fa918754ed65ddf6bbd9926ce31d75c61e
Dziad573 <krystiansmagacz05@gmail.com>
2024-04-03 14:36:09 +0200

    watch

--- Vue/src/components/HelloWorld.vue
+++ Vue/src/components/HelloWorld.vue
@@ -3,17 +3,28 @@
   <button @click='incrementPension'>++</button>
   <p>Your pension: {{pension}}</p>
 
-  <h2>Price: {{ state.price }}</h2>
+  <h2>Price: {{ price }}</h2>
   <button @click="makeOrder">Make next order</button>
-  <p>Quantity: {{ state.quantity }}</p>
-  <p>Order total price: {{ state.totalPrice }} {{  state.quantity < 14 ? "": " -5%"  }}</p>
-  <p>Tax {{ state.tax }}</p>
+  <p>Quantity: {{ quantity }}</p>
+  <p>Order total price: {{ totalPrice }} {{  quantity < 14 ? "": " -5%"  }}</p>
+  <p>Tax {{ tax }}</p>
+  <div>
+    <p>
+      You have <strong>{{ shares }}</strong> shares and their value is 
+      <strong>{{ sharesValue }} $</strong>, because share price is
+      <strong>{{ sharePrice }} $</strong>
+    </p>
+    <button @click="changeNumberOfShares(1)">Buy one share</button>
+    <button @click="changeNumberOfShares(5)">Buy five shares</button>
+    <button @click="changeNumberOfShares(-1)">Sell one share</button>
+    <button @click="changeNumberOfShares(-5)">Sell five shares</button>
+  </div>
 
 </template>
 
 
 <script>
-import { computed, reactive, ref } from 'vue';
+import { computed, reactive, toRefs, ref, watch } from 'vue';
 
 export default {
   name: 'HellowWorld',
@@ -24,6 +35,8 @@ export default {
     function incrementPension(){
       pension.value++;
     }
+
+    // reactive, computed
 
     const state = reactive({
       quantity: 10,
@@ -45,10 +58,37 @@ export default {
       state.quantity++;
     }
     
+    // watch
+
+    const shares = ref(15);
+    const sharePrice = ref(20);
+    const sharesValue = computed(() => shares.value * sharePrice.value)
+
+    function changeNumberOfShares(number){
+      if(shares.value + number >=0){
+        shares.value += number;
+      }
+
+    }
+
+    watch(shares, (shares, prevShares) => {
+      shares > prevShares ? getPrice (1, 5) : getPrice(-5, -1);
+    });
+
+    function getPrice(min, max){
+      const priceDiff = Math.floor(Math.random() * (max-min) + min);
+      if(sharePrice.value + priceDiff >= 0){
+        sharePrice.value += priceDiff;
+      }
+
+    }
     
 
 
-    return {pension, incrementPension, state, makeOrder};
+    return {
+      pension, incrementPension, ...toRefs(state), makeOrder, 
+      shares, sharePrice, sharesValue, changeNumberOfShares
+    };
   }
   
 
